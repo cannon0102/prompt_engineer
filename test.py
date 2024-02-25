055dc62ceb6fd901685f5e8c78d27fc1fa02b935
cannon0102 <yuzhoudan@qq.com>
2024-02-25 18:32:51 +0800

    test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+#encoding:utf-8
+################################################################################
+#
+# Copyright (c) 2024 cannon0102, Inc. All Rights Reserved
+#
+################################################################################
+"""
+This module provides ***
+Authors: yuzhoudan(yuzhoudan@qq.com)
+Date: 2024/02/25 18:32:41
+"""
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+if __name__ == '__main__':
+    pass
